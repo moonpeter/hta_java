00fcda51845f10f0257ba4e3482be4ad2de22b5b
moonpeter <moonpeter@kakao.com>
2020-11-17 17:51:24 +0900

    update 11/17

--- src/hta_java_01/Variable2.java
+++ src/hta_java_01/Variable2.java
@@ -6,7 +6,7 @@ public class Variable2 {
 		// 참조형-s1,s2는 메모리 주소, new는 연산자,
 		// new String("자바"); 에서 String()은 생성자
 		String s1 = "자바";
-		String s2 = new String("자바");
+		String s2 = new String("자바");	// String은 라이브러리로 제공되는 클래스
 		System.out.println("s1=" + s1 + " s2=" + s2);
 	}
 }
