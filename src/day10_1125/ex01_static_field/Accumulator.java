e4b6fede0ccc1b11260c8695a108799e69c1ff5a
moonpeter <moonpeter@kakao.com>
2020-11-25 10:32:32 +0900

    update 11/25

---
+++ src/day10_1125/ex01_static_field/Accumulator.java
@@ -0,0 +1,12 @@
+package day10_1125.ex01_static_field;
+
+public class Accumulator {
+    int total = 0;
+    static int grandTotal = 0;
+
+    void accumulate(int amount) {
+        total += amount;
+
+        grandTotal += amount;
+    }
+}
